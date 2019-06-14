2b38906485d533756dc01b76661b2d4829fd01e5
nevakee716 <nevakee716@gmail.com>
2019-06-14 10:17:23 +0200

    working on popout, and with the objectpage root object

--- src/cwFilterByContextualAssociation.js
+++ src/cwFilterByContextualAssociation.js
@@ -2,109 +2,96 @@
 /*global cwAPI, jQuery*/
 
 (function(cwApi, $) {
-    'use strict';
+    "use strict";
 
     var cwFilterByContextualAssociation = function(options, viewSchema) {
+        this.replaceLayout = options.CustomOptions['replace-layout'];
+        cwApi.extend(this, cwApi.cwLayouts[this.replaceLayout], options, viewSchema);
 
-        cwApi.extend(this, cwApi.cwLayouts.CwLayout, options, viewSchema);
         this.trueNodesID = {};
         this.falseNodesID = {};
-        this.createObjectNodes(true,this.options.CustomOptions['filter-in']);
-        this.createObjectNodes(false,this.options.CustomOptions['filter-out']);
-        this.replaceLayout = this.options.CustomOptions['replace-layout'];
-        this.topObject = this.options.CustomOptions['top-object'];
-        cwApi.extend(this, cwApi.cwLayouts[this.replaceLayout], options, viewSchema);
+        this.createObjectNodes(true, this.options.CustomOptions["filter-in"]);
+        this.createObjectNodes(false, this.options.CustomOptions["filter-out"]);
+        this.replaceLayout = this.options.CustomOptions["replace-layout"];
+        this.topObject = this.options.CustomOptions["top-object"];
+
         cwApi.registerLayoutForJSActions(this);
     };
 
-    cwFilterByContextualAssociation.prototype.drawOneMethod = function(output, child) {
-    	if(this.topObject) {
-    		this.findFilterNode(child,this.item,null);
-    	} else {
-    		this.findFilterNode(child,child,null);
-    	}
-        
-        if(cwApi.cwLayouts[this.replaceLayout].drawOneMethod) {
-            cwApi.cwLayouts[this.replaceLayout].drawOneMethod.call(this, output, child);
-        } else if(cwApi.cwLayouts[this.replaceLayout].drawOne) {
-            cwApi.cwLayouts[this.replaceLayout].drawOne.call(this, output, child);
+    cwFilterByContextualAssociation.prototype.drawAssociations = function(output, associationTitleText, object) {
+        function getUrlParam(name, url) {
+            if (!url) url = location.href;
+            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+            var regexS = "[\\?&]" + name + "=([^&#]*)";
+            var regex = new RegExp(regexS);
+            var results = regex.exec(url);
+            return results == null ? null : results[1];
+        }
+
+        if (this.topObject) {
+            let father = {};
+            father.object_id = parseInt(getUrlParam("cwid"), 10);
+            this.findFilterNode(object, father, null);
+        } else {
+            this.findFilterNode(object, null, null);
+        }
+
+        if (cwApi.cwLayouts[this.replaceLayout].prototype.drawAssociations) {
+            cwApi.cwLayouts[this.replaceLayout].prototype.drawAssociations.call(this, output, associationTitleText, object);
+        } else {
+            cwApi.cwLayouts.CwLayout.prototype.drawAssociations.call(this, output, associationTitleText, object);
         }
     };
 
-    cwFilterByContextualAssociation.prototype.createObjectNodes = function(nodeType,customOptions) {
+    cwFilterByContextualAssociation.prototype.createObjectNodes = function(nodeType, customOptions) {
         var nodes = customOptions.split(";");
         for (var i = 0; i < nodes.length; i += 1) {
-            if(nodeType === true) {
+            if (nodeType === true) {
                 this.trueNodesID[nodes[i]] = nodes[i];
-            } else if(nodeType === false){
+            } else if (nodeType === false) {
                 this.falseNodesID[nodes[i]] = nodes[i];
             }
         }
     };
 
-
-    cwFilterByContextualAssociation.prototype.findFilterNode = function(child,topfather) {
+    cwFilterByContextualAssociation.prototype.findFilterNode = function(child, topfather) {
         var nextChild = null;
         var i;
         var nodeToDelete;
-
+        var objectToUseAsContextualFilter;
         for (var associationNode in child.associations) {
             if (child.associations.hasOwnProperty(associationNode)) {
                 nodeToDelete = [];
                 for (i = 0; i < child.associations[associationNode].length; i += 1) {
-                    if(this.trueNodesID.hasOwnProperty(associationNode) && child.associations[associationNode][i].name === topfather.name) {
+                    let nextChild = child.associations[associationNode][i];
+                    if (this.trueNodesID.hasOwnProperty(associationNode) && topfather && nextChild.object_id === topfather.object_id) {
                         return true;
-                    } else if(this.falseNodesID.hasOwnProperty(associationNode) && child.associations[associationNode][i].name === topfather.name) {
+                    } else if (this.falseNodesID.hasOwnProperty(associationNode) && topfather && nextChild.object_id === topfather.object_id) {
                         return false;
                     } else {
-                        nextChild = child.associations[associationNode][i];
-                        if(!this.findFilterNode(nextChild,topfather)) {
+                        if(topfather === null) objectToUseAsContextualFilter = nextChild ;
+                        else objectToUseAsContextualFilter = topfather; 
+
+                        if (!this.findFilterNode(nextChild, objectToUseAsContextualFilter) ) {
                             nodeToDelete.push(i);
                         }
                     }
                 }
-                if(this.trueNodesID.hasOwnProperty(associationNode)) {
+                if (this.trueNodesID.hasOwnProperty(associationNode)) {
                     return false;
                 }
-                if(this.falseNodesID.hasOwnProperty(associationNode)) {
+                if (this.falseNodesID.hasOwnProperty(associationNode)) {
                     return true;
                 }
-                for (i = nodeToDelete.length-1; i >= 0; i -= 1) {
+                for (i = nodeToDelete.length - 1; i >= 0; i -= 1) {
                     delete child.associations[associationNode].splice(nodeToDelete[i], 1);
                 }
             }
         }
 
-        for (var iAssociationNode in child.iAssociations) {
-            if (child.iAssociations.hasOwnProperty(iAssociationNode)) {
-                nodeToDelete = [];
-                for (i = 0; i < child.iAssociations[iAssociationNode].length; i += 1) {
-                    if(this.trueNodesID.hasOwnProperty(iAssociationNode) && child.iAssociations[iAssociationNode][i].name === topfather.name) {
-                        return true;
-                    } else if(this.falseNodesID.hasOwnProperty(iAssociationNode) && child.iAssociations[iAssociationNode][i].name === topfather.name) {
-                        return false;
-                    } else {
-                        nextChild = child.iAssociations[iAssociationNode][i];
-                        if(!this.findFilterNode(nextChild,topfather)) {
-                            nodeToDelete.push(i);
-                        }
-                    }
-                }
-                if(this.trueNodesID.hasOwnProperty(iAssociationNode)) {
-                    return false;
-                }
-                if(this.falseNodesID.hasOwnProperty(iAssociationNode)) {
-                    return true;
-                }
-                for (i = nodeToDelete.length-1; i >= 0; i -= 1) {
-                    delete child.iAssociations[iAssociationNode].splice(nodeToDelete[i], 1);
-                }
-            }
-        }
+
         return true;
     };
 
-
     cwApi.cwLayouts.cwFilterByContextualAssociation = cwFilterByContextualAssociation;
-
-    }(cwAPI, jQuery));
+})(cwAPI, jQuery);
